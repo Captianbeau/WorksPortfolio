70cc8f6a22f780ca205a56bcac4013c2c7b8a4e9
Capti <captianbeau@gmail.com>
2024-04-13 17:36:13 -0600

    added avatar to about me

--- WorksPortfolio/src/pages/AboutMe.jsx
+++ WorksPortfolio/src/pages/AboutMe.jsx
@@ -1,7 +1,9 @@
+import Avatar from '../assets/images/avatarPortNew.jpg'
 export default function AboutMe() {
     //need a profile pic
     return <div>
         <h2>About Me</h2>
+        <img src={Avatar} alt='Avatar' />
         <p>Blah</p>
     </div>
 }
