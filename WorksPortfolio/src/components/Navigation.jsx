3fb74b75c5ff197457f3b60131d93bee3e0fc9cc
Capti <captianbeau@gmail.com>
2024-04-13 19:53:19 -0600

    added links to navigation

--- WorksPortfolio/src/components/Navigation.jsx
+++ WorksPortfolio/src/components/Navigation.jsx
@@ -1,9 +1,27 @@
 //links to aboutme page, portfolio, contact, resume page
-import {Link, useLocation} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom'; 
 function Navigation (){
+    const currentPage = useLocation().pathname;
 return <div>
     <nav>
-        
+        <Link
+        to= "/"
+        className={currentPage === '/' ? 'nav active': 'navLink'}
+        >
+            About Me
+        </Link>
+        <Link
+        to= "/Works"
+        className={currentPage === '/Works' ? 'nav active': 'navLink'}
+        >
+            My Work
+        </Link>
+        <Link
+        to= "/Resume"
+        className={currentPage === '/Resume' ? 'nav active': 'navLink'}
+        >
+            My Resume
+        </Link>
     </nav>
 </div>
 };
