df4982c5cbb8bffaa8913582de65e0b8fd37b01a
Capti <captianbeau@gmail.com>
2024-04-14 17:29:01 -0600

    restructured stylings and added to nav stylings

--- WorksPortfolio/src/components/Navigation.jsx
+++ WorksPortfolio/src/components/Navigation.jsx
@@ -1,29 +1,33 @@
 //links to aboutme page, portfolio, contact, resume page
-import {Link, useLocation} from 'react-router-dom'; 
-function Navigation (){
+import { Link, useLocation } from 'react-router-dom';
+function Navigation() {
     const currentPage = useLocation().pathname;
-return <div>
-    <nav>
-        <Link
-        to= "/"
-        className={currentPage === '/' ? 'nav active': 'navLink'}
-        >
-            About Me
-        </Link>
-        <Link
-        to= "/Works"
-        className={currentPage === '/Works' ? 'nav active': 'navLink'}
-        >
-            My Work
-        </Link>
-        <Link
-        to= "/Resume"
-        className={currentPage === '/Resume' ? 'nav active': 'navLink'}
-        >
-            My Resume
-        </Link>
-    </nav>
-</div>
+    return <div>
+        <nav>
+            <Link
+                to="/"
+                className={currentPage === '/' ? 'nav active' : 'navLink'}
+            >
+                About Me
+            </Link>
+            <Link
+                to="/Works"
+                className={currentPage === '/Works' ? 'nav active' : 'navLink'}
+            >
+                My Work
+            </Link>
+            <Link
+                to="/Resume"
+                className={currentPage === '/Resume' ? 'nav active' : 'navLink'}
+            >
+                My Resume
+            </Link>
+            <Link
+                to="/Contact"
+                className={currentPage === '/Contact' ? 'nav active' : 'navLink'}>
+                Contact</Link>
+        </nav>
+    </div>
 };
 
 export default Navigation;
