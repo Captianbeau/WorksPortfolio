dc1d07e21d4dadb5acc9b0da6cedd32f831ac881
Capti <captianbeau@gmail.com>
2024-04-14 17:10:11 -0600

    started on styling and fixed header

--- WorksPortfolio/src/pages/AboutMe.jsx
+++ WorksPortfolio/src/pages/AboutMe.jsx
@@ -1,9 +1,15 @@
 import Avatar from '../assets/images/avatarPortNew.jpg'
 export default function AboutMe() {
     //need a profile pic
-    return <div>
+    return <div className='aboutMe'>
         <h2>About Me</h2>
         <img src={Avatar} alt='Avatar' className= "avatar" />
-        <p>Blah</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+             when an unknown printer took a galley of type and scrambled it to make a 
+             type specimen book. It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was popularised in the
+               1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
+                more recently with desktop publishing software like Aldus PageMaker including 
+                versions of Lorem Ipsum.</p>
     </div>
 }
