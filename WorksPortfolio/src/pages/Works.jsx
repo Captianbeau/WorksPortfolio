fdb50a0e17407efb0da65fc45a48d1bd23038ec3
Capti <captianbeau@gmail.com>
2024-04-15 15:45:07 -0600

    added git logo to works and reworked page

--- WorksPortfolio/src/pages/Works.jsx
+++ WorksPortfolio/src/pages/Works.jsx
@@ -3,55 +3,80 @@ import Schedule from '../assets/images/Scheduler.png';
 import Quiz from '../assets/images/Quiz.png';
 import Garden from '../assets/images/GardenPlanner.png';
 import Books from '../assets/images/BooksRefactor.png';
+import GitLogo from '../assets/images/github-mark.png';
 export default function Works() {
     return <div>
         <h2>Works</h2>
         <div>
-            <div>
-                <h3>Weather App</h3>
+            <h3>Weather App</h3>
+            <div className='works'>
+
                 <a href='https://captianbeau.github.io/WeatherApp/'>
-                <img src={Weather} alt='Website Screenshot' />
+                    <img src={Weather} alt='Website Screenshot' />
                 </a>
-                <p className='description'>description</p>
+                <a href='https://github.com/Captianbeau/WeatherApp'>
+                    <img src={GitLogo} alt='GitHub Logo' />
+                </a>
             </div>
-            
-            <a href='https://github.com/Captianbeau/WeatherApp'>Git Hub</a>
+
+
         </div>
         <div>
-            <div>
-            <h3></h3>
-            <a href='https://captianbeau.github.io/Scheduler/'><img src={Schedule} alt='Website Screenshot' /></a>
-                
+            <h3>Scheduler</h3>
+            <div className='works'>
+
+                <a href='https://captianbeau.github.io/Scheduler/'>
+                    <img src={Schedule} alt='Website Screenshot' />
+                </a>
+                <a href='https://github.com/Captianbeau/Scheduler'>
+                    <img src={GitLogo} alt='GitHub Logo' />
+                </a>
             </div>
-            
-            <a  href='https://github.com/Captianbeau/Scheduler'>Git Hub</a>
+
+
         </div>
         <div>
-            <div>
-            <h3></h3>
-                <img src={Quiz} alt='Website Screenshot'/>
-                <p className='description'>description</p>
+            <h3>Quiz</h3>
+            <div className='works'>
+
+                <a href='https://captianbeau.github.io/Quiz-DarbiesADHD/'>
+                    <img src={Quiz} alt='Website Screenshot' />
+                </a>
+                <a href='https://github.com/Captianbeau/Quiz-DarbiesADHD'>
+                    <img src={GitLogo} alt='GitHub Logo' />
+                </a>
+
             </div>
-            <a href='https://captianbeau.github.io/Quiz-DarbiesADHD/'>Quiz</a>
-            <a href='https://github.com/Captianbeau/Quiz-DarbiesADHD'>Git Hub</a>
+
+
         </div>
         <div>
-            <div>
-            <h3></h3>
-                <img src={Garden} alt='Website Screenshot'/>
-                <p className='description'>description</p>
+            <h3>Garden Planner</h3>
+            <div className='works'>
+
+                <a href='https://captianbeau.github.io/Garden-Planner/'>
+                    <img src={Garden} alt='Website Screenshot' />
+                </a>
+                <a href='https://github.com/Captianbeau/Garden-Planner'>
+                    <img src={GitLogo} alt='GitHub Logo' />
+                </a>
+
             </div>
-            <a href='https://captianbeau.github.io/Garden-Planner/'>Garden Planner</a>
-            <a href='https://github.com/Captianbeau/Garden-Planner'>Git Hub</a>
+
+
         </div>
         <div>
-            <div>
-            <h3></h3>
-                <img src={Books} alt='Website Screenshot'/>
-                <p className='description'>description</p>
+            <h3>Google Books</h3>
+            <div className='works'>
+
+                <a href='https://booksrefactor.onrender.com/'>
+                    <img src={Books} alt='Website Screenshot' />
+                </a>
+                <a href='https://github.com/Captianbeau/BooksRefactor'>
+                    <img src={GitLogo} alt='GitHub Logo' />
+                </a>
             </div>
-            <a href='https://booksrefactor.onrender.com/'>Google Books</a>
-            <a href='https://github.com/Captianbeau/BooksRefactor'>Git Hub</a>
+
         </div>
     </div>
 }
