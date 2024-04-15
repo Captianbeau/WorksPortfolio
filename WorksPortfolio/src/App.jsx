7581b97aceced26e2d0ecbf55f1b3ad49a743f3d
Capti <captianbeau@gmail.com>
2024-04-15 16:44:31 -0600

    deleted file contactform from components

--- WorksPortfolio/src/App.jsx
+++ WorksPortfolio/src/App.jsx
@@ -1,9 +1,8 @@
 //about me page
-import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import AboutMe from './pages/AboutMe';
+
 import './App.css'
 
 function App() {
