f33df394244f72c043aff4a950997d6584bf1e83
Capti <captianbeau@gmail.com>
2024-04-15 16:41:01 -0600

    added functionallity to contact form

--- WorksPortfolio/src/pages/ContactForm.jsx
+++ WorksPortfolio/src/pages/ContactForm.jsx
@@ -1,20 +1,43 @@
+import {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 export default function Contact () {
+    []
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_3uvhu67', 'template_gv48yvq', form.current, {
+            publicKey: 'oAk9I3pt8MSpkeAs4',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
     return <div>
         <h2>Contact Me</h2>
-        <form className='form'>
+        <form className='form' ref={form} onSubmit={sendEmail}>
+        <input
+            name="user_name"
+            type="text"
+            placeholder="Your Name"
+            />
             <input
             name="email"
             type="text"
-            placeholder="email"
+            placeholder="Email"
             />
-        </form>
-        <form className='form'>
             <input
             name="message"
             type="text"
-            placeholder="Your message"
+            placeholder="Your Message"
             />
-            <button type="submit">
+            <button type="submit" value="Send">
                 Submit
             </button>
         </form>
