8ff43dedcc788c2debdca9e1056befbb5c2c875f
Capti <captianbeau@gmail.com>
2024-04-14 18:08:28 -0600

    renamed images and imported them into works

--- WorksPortfolio/src/pages/Works.jsx
+++ WorksPortfolio/src/pages/Works.jsx
@@ -1,5 +1,13 @@
+import Weather from '../assets/images/weatherApp.png';
+import Schedule from '../assets/images/Scheduler.png';
+import Quiz from '../assets/images/Quiz.png';
+import Garden from '../assets/images/GardenPlanner.png';
+import Books from '../assets/images/BooksRefactor.png';
 export default function Works (){
 return <div>
     <h2>Works</h2>
+    <div>
+        
+    </div>
 </div>
 }
