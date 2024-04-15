517ac7b00fa51de562050fa1c437e3d87ce1feed
Capti <captianbeau@gmail.com>
2024-04-15 17:15:31 -0600

    added link to resumeEx in resume

--- WorksPortfolio/src/pages/Resume.jsx
+++ WorksPortfolio/src/pages/Resume.jsx
@@ -1,12 +1,17 @@
-export default function Resume () {
+export default function Resume() {
+    //https://drive.google.com/uc?export=download&id=16XO_Mdr16u8hGbUDuyFMb_G-ehk43xSvr50IP_rNxwU
     return <div>
+        <a href='https://docs.google.com/document/d/1CPKyIxbMFQzcYLExaPGDr0Z9isyOHPZZ5TOUgOYSLss/edit?usp=sharing'>
+            <button type="button">Resume Doc</button>
+        </a>
+
         <h2>Resume</h2>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-             when an unknown printer took a galley of type and scrambled it to make a 
-             type specimen book. It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was popularised in the
-               1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
-                more recently with desktop publishing software like Aldus PageMaker including 
-                versions of Lorem Ipsum.</p>
+            when an unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the
+            1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
+            more recently with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.</p>
     </div>
 }
